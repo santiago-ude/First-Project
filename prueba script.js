6909e1ff4fda4b538088f21b0aa36ea26e3a5ae9
Santiago <santiude2004@gmail.com>
2022-11-09 18:51:58 -0300

    agregué la carpeta de la prueba de js

---
+++ prueba script.js
@@ -0,0 +1,119 @@
+// VARIABLE
+
+let una = 4;
+
+let dos = "Holisss";
+
+// MODIFICAR VARIABLE (modifica la variable x escala)
+
+una = 3;
+
+dos = "Chauchisss";
+
+// CONSTANTE (No se modifica)
+
+const pito = 6;
+
+// OPERADORES MATEMATICOS (En la Consola)
+
+// SUMA
+console.log(A+B); 
+// RESTA
+console.log(A-B);
+// MULTIPLICACION
+console.log(A*B); 
+// DIVISION
+console.log(A/B);
+// REST DE LA DIVISION
+console.log(A%B); 
+
+// OPERADORES DE INCREMENTO Y DECREMENTO 
+
+// RESTA INTERNAMENTE Y EN EL PROXIMO RESULTADO MUESTRA LA RESTA HECHA
+console.log(a--); 
+
+// RESTA DE UNA 
+console.log(--a)
+
+// SUMA INTERNAMENTE Y EN EL PROXIMO RESULTADO MUESTRA LA SUMA
+console.log(a++)
+
+// SUMA DE UNA 
+console.log(++a)
+
+// OPERADORES DE ASIGNACION
+// (queres modificar una variante)
+
+a+=3;  ES IGUAL QUE a = a+3;
+a-=3;  ES IGUAL QUE a = a-3;
+a*=3;  ES IGUAL QUE a = a*3;
+a/=3;  ES IGUAL QUE a = a/3;
+a%=3;  ES IGUAL QUE a = a%3;
+
+
+
+// STRINGS 
+
+
+
+// DEVUELVE LA PALABRA EN MAYUSCULA
+console.log(a.toUpperCase())
+
+// DEVUELVE LA PALABRA EN MINUSCULA
+console.log(a.toLowerCase())
+
+// BUSCA POR LETRA O PALABRA Y TE MARCA EL NUMERO DE CARACTER DONBDE SE UBICA
+console.log(a.indexOf("o"-"hola"))
+
+// REMPLAZA LA PALABRA QUE PONES EN UNA VARIABLE EN ESPECIFICO POR LA PALABRA QUE LE PIDAS
+console.log(a.replace("Hola","Buenas"))
+
+// SELECCIONA DEL NUMERO DE CARACTER AL NUMERO DE CARACTER QUE LE DIGAMOS
+console.log(a.substring(3,9))
+console.log(a.slice(-3))}
+
+// ELIMINA LOS ESPACIOS QUE QUEDEN O SE GENEREN  AL ARRANQUE O FINAL DE LA JUGADA
+console.log(a.trim())
+
+// SIRVE PARA VER SI LA VARIABLE TERMINA O EMPIEZA EN ESA LETRA O PALABRA
+console.log(a.starwith("Hola"-"H")) se le puede proponer que busque a partir de un caracter
+console.log(a.Endswith("Mundo"-"M"))
+console.log(a.startwith("Mundo",4)) A partir de la posicion 4
+console.log(a.startwith("Hola",0)) A partir de la posicion 0
+
+// SIRVE PARA BUSCAR SI EN LA CADENA HAY DE ALGUNA LETRA O PALABRA EN ESPECFICO (True o False)
+console.log(a.includes("H"))
+console.log(a.include("Hola",))
+console.log(a.include("M",4)) despues de la posicion 4
+console.log(a.include("Mundo",4))
+
+
+// SIRVE PARA QUE SE REPITA ESTA CADENA UNA CANTIDAD DE VECES DETERMINADA
+console.log(a.repeat(3))
+console.log("Hola".repeat(3)) REPITE "Hola" 3 VECES
+console.log(cadena, "Hola".repeat(3)) PONE LA CADENA Y REPITE 3 VECES "hola".
+
+// PARA ESCRIBIR SIN TENER QUE HACER ESTO
+
+console.log("Hola " + nombre + apellido + " vos tenes " + edad + )
+
+// SE PUEDE HACER ASI
+console.log(`Hola ${nombre} ${apellido} tenes ${edad} años.`)
+
+
+
+
+
+// MATH 
+
+
+console.log(Math.abs(num})) TE DA EL MODULO O NUMERO SIN IMPORTAR EL SIGNO (9).
+console.log(Math.ceil(num)) REDONDEA PARA ARRIBA SIN IMPORTAR EL DECIMAL.
+console.log(Math.floor(num)) REDONDEA PARA ABAJO SIN IMPORTAR EL DECIMAL.
+console.log(Math.pow(2,4)) POTENCIA EL PRIMER NUMERO ELEVADO AL SEGUNDO.
+console.log(Math.random) NUMERO RANDOM DEL 0 AL 1.
+console.log(Math.round(Math.random)* (max - min) + min) NUMERO ENTERO RANDOM ENTRE TAL Y TAL NUMERO.
+console.log(Math.sign(-5 o 5)) TE DA -1 A NUMEROS NEGATIVOS Y 1 A NUMEROS POSITIVOS Y 0 A 0.
+console.log(Math.sqrt(100)) TE DEVUELVE LA RAIZ CUADRADA DEL NUMERO QUE QUERRAMOS.
+
+
